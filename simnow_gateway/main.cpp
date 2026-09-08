5592b7cff7cb7b0d9861484fe91dcb05b5383f74
itsadrianxv <adrian@adrianxv.cn>
2026-09-08 18:24:00 +0800

    initial commit

---
+++ simnow_gateway/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main(int argc, char**) {
+  std::cout << "simnow_gateway: CTP adapter placeholder (SDK integration pending)\n";
+  if (argc < 2) std::cerr << "usage: simnow_gateway CONFIG.toml\n";
+  return 0;
+}
